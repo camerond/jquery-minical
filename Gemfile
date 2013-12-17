3c29abad1725969bf0c11a7ae30d471814427c0a
Cameron Daigle <camerondaigle@gmail.com>
2013-12-17 12:39:27 -0500

    Prep for gh-pages deploy

--- Gemfile
+++ Gemfile
@@ -1,7 +1,4 @@
 source 'http://rubygems.org'
 
 gem "middleman", "~>3.1.2"
-gem "middleman-livereload", "~> 3.1.0"
-gem "middleman-syntax"
 gem "middleman-deploy"
-gem "redcarpet"
--- config.rb
+++ config.rb
@@ -1,1 +1,8 @@
 activate :livereload
+activate :relative_assets
+activate :directory_indexes
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+end
