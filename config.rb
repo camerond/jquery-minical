3c0787dfa51e63252b45adc4de019f9e9f777494
Cameron Daigle <camerondaigle@gmail.com>
2014-03-14 15:09:04 -0400

    Turn off sass line comments

--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@ activate :livereload
 activate :relative_assets
 activate :directory_indexes
 
+set :sass, :style => :expanded, :line_comments => false
+
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
