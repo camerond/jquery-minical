197b28d3740bda5301a85f67c0e3571b04566830
Cameron Daigle <camerondaigle@gmail.com>
2014-03-18 13:53:33 -0400

    Update plain js

--- source/javascripts/jquery.minical.plain.js
+++ source/javascripts/jquery.minical.plain.js
@@ -126,6 +126,7 @@
         $li.find(".minical_next").detach();
       }
       this.$cal.empty().append($li);
+      this.markSelectedDay();
       this.fireCallback('month_drawn');
       return this.$cal;
     },
@@ -168,6 +169,7 @@
     },
     selectDay: function(date) {
       this.selected_day = date;
+      this.markSelectedDay();
       this.$el.val(this.date_format(this.selected_day));
       return this.fireCallback('date_changed');
     },
@@ -236,7 +238,6 @@
       if (this.$cal.is(":visible") || this.$el.is(":disabled")) {
         return;
       }
-      this.markSelectedDay();
       this.highlightDay(this.selected_day);
       this.positionCalendar().show();
       this.attachCalendarEvents();
